ead5e0d81f5e2110be12426e1ead051fea2d4a5a
Pigeon <fabrizioapuzzo2003@gmail.com>
2025-03-09 19:44:37 +0100

    bug fix

--- src/main/java/com/dieti/dietiestatesbackend/DietiEstatesBackend.java
+++ src/main/java/com/dieti/dietiestatesbackend/DietiEstatesBackend.java
@@ -123,7 +123,7 @@ public class DietiEstatesBackend {
 		email=email.toLowerCase();
 		try
 		{
-			String query = "SELECT * FROM DietiEstates2025.utente WHERE email = ? AND password = ?";
+			String query = "SELECT * FROM \"DietiEstates2025\".utente WHERE email = ? AND password = ?";
 			PreparedStatement ps = myConnection.prepareStatement(query);
 			ps.setString(1, email);
 			ps.setString(2, password);
@@ -143,7 +143,7 @@ public class DietiEstatesBackend {
 		email=email.toLowerCase();
 		try
 		{
-			String query = "SELECT * FROM DietiEstates2025.utente WHERE email = ?";
+			String query = "SELECT * FROM \"DietiEstates2025\".utente WHERE email = ?";
 			PreparedStatement ps = myConnection.prepareStatement(query);
 			ps.setString(1, email);
 			
