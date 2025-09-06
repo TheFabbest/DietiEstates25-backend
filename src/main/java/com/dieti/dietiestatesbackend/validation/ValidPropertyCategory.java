94afe404a680573a8c17b21383029bb8a2a73065
Luca Barrella <lucabarrella@icloud.com>
2025-09-06 11:12:38 +0200

    refactor: Remove unused import for ValidPropertyCategoryValidator in ValidPropertyCategory

--- src/main/java/com/dieti/dietiestatesbackend/validation/ValidPropertyCategory.java
+++ src/main/java/com/dieti/dietiestatesbackend/validation/ValidPropertyCategory.java
@@ -7,7 +7,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
-import com.dieti.dietiestatesbackend.validation.ValidPropertyCategoryValidator;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
