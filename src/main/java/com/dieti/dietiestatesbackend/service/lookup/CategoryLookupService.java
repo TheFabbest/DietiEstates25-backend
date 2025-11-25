5a44acd2b40074f1a785db01056f33948eded713
humorpeak <marydv1804@gmail.com>
2025-11-25 16:21:55 +0100

    sonarqube: rimozione annotazione inutile cacheable su classe astratta

--- src/main/java/com/dieti/dietiestatesbackend/service/lookup/CategoryLookupService.java
+++ src/main/java/com/dieti/dietiestatesbackend/service/lookup/CategoryLookupService.java
@@ -5,8 +5,6 @@ import com.dieti.dietiestatesbackend.entities.PropertyCategory;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.cache.annotation.Cacheable;
-
 /**
  * Service interface per il lookup delle PropertyCategory.
  * Updated to support the new canonical naming approach.
@@ -16,7 +14,6 @@ public interface CategoryLookupService {
      * Find a PropertyCategory by its specific name (e.g., "Apartment", "Villa", "Office").
      * This is the primary lookup method according to the new architecture.
      */
-    @Cacheable
     Optional<PropertyCategory> findByName(String name);
     
     /**
