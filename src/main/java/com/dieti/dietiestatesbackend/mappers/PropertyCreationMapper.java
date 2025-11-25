30bef10c2f35d053dbb165783408349ded58ca31
humorpeak <marydv1804@gmail.com>
2025-11-25 14:41:51 +0100

    rimossa injection sonarqube e riordinato il file

--- src/main/java/com/dieti/dietiestatesbackend/mappers/PropertyCreationMapper.java
+++ src/main/java/com/dieti/dietiestatesbackend/mappers/PropertyCreationMapper.java
@@ -1,7 +1,4 @@
 package com.dieti.dietiestatesbackend.mappers;
-import com.dieti.dietiestatesbackend.dto.request.AddressRequest;
-import com.dieti.dietiestatesbackend.entities.Address;
-import org.mapstruct.Qualifier;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -10,26 +7,28 @@ import java.lang.annotation.Target;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.dieti.dietiestatesbackend.service.AddressService;
- 
+import org.mapstruct.Qualifier;
+
+import com.dieti.dietiestatesbackend.dto.request.AddressRequest;
 import com.dieti.dietiestatesbackend.dto.request.CreateCommercialPropertyRequest;
 import com.dieti.dietiestatesbackend.dto.request.CreateGaragePropertyRequest;
 import com.dieti.dietiestatesbackend.dto.request.CreateLandPropertyRequest;
 import com.dieti.dietiestatesbackend.dto.request.CreateResidentialPropertyRequest;
+import com.dieti.dietiestatesbackend.entities.Address;
 import com.dieti.dietiestatesbackend.entities.CommercialProperty;
+import com.dieti.dietiestatesbackend.entities.Contract;
 import com.dieti.dietiestatesbackend.entities.Garage;
 import com.dieti.dietiestatesbackend.entities.Land;
+import com.dieti.dietiestatesbackend.entities.PropertyCategory;
 import com.dieti.dietiestatesbackend.entities.ResidentialProperty;
 import com.dieti.dietiestatesbackend.entities.User;
-import com.dieti.dietiestatesbackend.entities.Contract;
-import com.dieti.dietiestatesbackend.entities.PropertyCategory;
 import com.dieti.dietiestatesbackend.exception.EntityNotFoundException;
+import com.dieti.dietiestatesbackend.service.AddressService;
 import com.dieti.dietiestatesbackend.service.lookup.AgentLookupService;
 import com.dieti.dietiestatesbackend.service.lookup.CategoryLookupService;
 import com.dieti.dietiestatesbackend.service.lookup.ContractLookupService;
 import com.dieti.dietiestatesbackend.service.lookup.HeatingLookupService;
- 
+
 /**
  * Mapper MapStruct per la creazione di Property dalle request.
  * Convertito in classe astratta per permettere l'iniezione dei servizi di lookup necessari
@@ -38,14 +37,23 @@ import com.dieti.dietiestatesbackend.service.lookup.HeatingLookupService;
 @Mapper(componentModel = "spring", uses = {AgentLookupService.class, CategoryLookupService.class, ContractLookupService.class, HeatingLookupService.class, AddressMapper.class})
 public abstract class PropertyCreationMapper {
 
-    @Autowired
-    protected ContractLookupService contractLookupService;
+    protected final ContractLookupService contractLookupService;
+    protected final CategoryLookupService categoryLookupService;
+    protected final AddressMapper addressMapper;
+    protected final AddressService addressService;
 
-    @Autowired
-    protected CategoryLookupService categoryLookupService;
+    protected PropertyCreationMapper(
+            ContractLookupService contractLookupService,
+            CategoryLookupService categoryLookupService,
+            AddressMapper addressMapper,
+            AddressService addressService
+    ) {
+        this.contractLookupService = contractLookupService;
+        this.categoryLookupService = categoryLookupService;
+        this.addressMapper = addressMapper;
+        this.addressService = addressService;
+    }
 
-    @Autowired
-    protected AddressMapper addressMapper;
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "agent", source = "agent")
@@ -55,14 +63,14 @@ public abstract class PropertyCreationMapper {
     @Mapping(target = "heating", source = "request.heatingType")
     @Mapping(target = "numberOfFloors", source = "request.numberOfFloors")
     @Mapping(target = "yearBuilt", source = "request.yearBuilt")
-    @Mapping(target = "floor", source = "request.floor") // Mappa il campo floor
+    @Mapping(target = "floor", source = "request.floor")
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     @Mapping(target = "additionalFeatures", ignore = true)
     @Mapping(target = "imageDirectoryUlid", ignore = true)
     @Mapping(target = "numberOfImages", ignore = true)
     public abstract ResidentialProperty toEntity(CreateResidentialPropertyRequest request, User agent);
- 
+
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "agent", source = "agent")
     @Mapping(target = "contract", source = "request.contractType")
@@ -103,7 +111,7 @@ public abstract class PropertyCreationMapper {
     @Mapping(target = "imageDirectoryUlid", ignore = true)
     @Mapping(target = "numberOfImages", ignore = true)
     public abstract Garage toEntity(CreateGaragePropertyRequest request, User agent);
-    
+
 
     /**
      * Metodo helper che MapStruct utilizza per mappare un nome di contratto (String) in un'entità Contract.
@@ -114,8 +122,6 @@ public abstract class PropertyCreationMapper {
         return contractLookupService.findByName(name)
                 .orElseThrow(() -> new EntityNotFoundException("Contract '" + name + "' not found"));
     }
-    @Autowired
-    protected AddressService addressService;
 
     @AddressMapping
     public Address mapAddress(AddressRequest addressRequest) {
@@ -128,7 +134,7 @@ public abstract class PropertyCreationMapper {
     @Retention(RetentionPolicy.CLASS)
     public @interface AddressMapping {
     }
- 
+
     /**
      * Metodo helper che MapStruct utilizza per mappare un nome di categoria (String) in un'entità PropertyCategory.
      * L'implementazione gestisce il caso nullo e lancia un'eccezione se la categoria non viene trovata.
